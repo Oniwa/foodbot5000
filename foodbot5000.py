350e480501eaa01186908b585a11b30efd9f5712
John Barksdale <jdbarks@gmail.com>
2017-07-19 18:36:54 -0500

    Will only respond to new tweets now

--- foodbot5000.py
+++ foodbot5000.py
@@ -21,33 +21,37 @@ class TwitterBot(object):
 
     def run_bot(self):
         print('running')
-        # Get a list of all the tweets where this account is mentioned
-        twts = self.api.mentions_timeline()
+        while(True):
+            # Get a list of all the tweets where this account is mentioned
+            twts = self.api.mentions_timeline(since_id=self.max_id)
 
-        # Get accounts current screen name
-        my_screen_name = self.api.me().screen_name
+            # Get accounts current screen name
+            my_screen_name = self.api.me().screen_name
 
-        # Phrases to recognize
-        phrase = ['@{} Where to eat?'.format(my_screen_name),
-                  '@{} where to eat?'.format(my_screen_name),
-                  ]
+            # Phrases to recognize
+            phrase = ['@{} Where to eat?'.format(my_screen_name),
+                      '@{} where to eat?'.format(my_screen_name),
+                      ]
 
-        for s in twts:
-            for i in phrase:
-                if i == s.text:
-                    print('Found something!')
-                    tweeters_screen_name = s.user.screen_name
-                    self.max_id = s.id
-                    try:
-                        self.api.update_status('@{} You should eat at'
-                                               ' {}'.format(tweeters_screen_name,
-                                                            pick_restaurant()),
-                                                            s.id)
-                        print('did something')
-                    except tw.error.TweepError as e:
-                        print(e)
+            for s in twts:
+                for i in phrase:
+                    if i == s.text:
+                        print('Found something!')
+                        tweeters_screen_name = s.user.screen_name
+                        self.max_id = s.id
+                        try:
+                            self.api.update_status('@{} You should eat at'
+                                                   ' {}'.format(tweeters_screen_name,
+                                                                pick_restaurant()),
+                                                                s.id)
+                            print('did something')
+                        except tw.error.TweepError as e:
+                            print(e)
+            time.sleep(300)
+
 
 if __name__ == '__main__':
     foodbot = TwitterBot(keys['consumer_key'], keys['consumer_secret'],
-                         keys['access_token'], keys['access_token_secret'], 0)
+                         keys['access_token'], keys['access_token_secret'], 887804597154312191)
     foodbot.run_bot()
+
