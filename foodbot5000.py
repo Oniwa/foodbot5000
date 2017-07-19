13e705b1e32a4240508b87049682751d16b62ecb
John Barksdale <jdbarks@gmail.com>
2017-07-19 18:15:41 -0500

    Changed to an OOP style

--- foodbot5000.py
+++ foodbot5000.py
@@ -4,36 +4,50 @@ import time
 from keys import keys
 from restaurant_picker import pick_restaurant
 
-# Get Keys from the secret key file
-CONSUMER_KEY = keys['consumer_key']
-CONSUMER_SECRET = keys['consumer_secret']
-ACCESS_TOKEN = keys['access_token']
-ACCESS_TOKEN_SECRET = keys['access_token_secret']
+class TwitterBot(object):
+    def __init__(self, consumer_key, consumer_secret,
+                 access_token, access_token_secret, max_id):
+        self.consumer_key = consumer_key
+        self.consumer_secret = consumer_secret
+        self.access_token = access_token
+        self.access_token_secret = access_token_secret
+        self.max_id = max_id
 
-# Authorize app with twitter
-auth = tw.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
-api = tw.API(auth)
+        # Authorize app with twitter
+        self.auth = tw.OAuthHandler(self.consumer_key, self.consumer_secret)
+        self.auth.set_access_token(self.access_token, self.access_token_secret)
+        self.api = tw.API(self.auth)
+        print('Authorized')
 
-# Get a list of all the tweets where this account is mentioned
-twts = api.mentions_timeline()
+    def run_bot(self):
+        print('running')
+        # Get a list of all the tweets where this account is mentioned
+        twts = self.api.mentions_timeline()
 
-# Get accounts current screen name
-my_screen_name = api.me().screen_name
+        # Get accounts current screen name
+        my_screen_name = self.api.me().screen_name
 
-# Phrases to recognize
-phrase = ['@{} Where to eat?'.format(my_screen_name),
-          '@{} where to eat?'.format(my_screen_name),
-          ]
+        # Phrases to recognize
+        phrase = ['@{} Where to eat?'.format(my_screen_name),
+                  '@{} where to eat?'.format(my_screen_name),
+                  ]
 
-for s in twts:
-    for i in phrase:
-        if i == s.text:
-            tweeters_screen_name = s.user.screen_name
-            max_id = s.id
-            try:
-                api.update_status('@{} You should eat at'
-                                  ' {}'.format(tweeters_screen_name,
-                                               pick_restaurant()), s.id)
-            except tw.error.TweepError as e:
-                print(e)
+        for s in twts:
+            for i in phrase:
+                if i == s.text:
+                    print('Found something!')
+                    tweeters_screen_name = s.user.screen_name
+                    self.max_id = s.id
+                    try:
+                        self.api.update_status('@{} You should eat at'
+                                               ' {}'.format(tweeters_screen_name,
+                                                            pick_restaurant()),
+                                                            s.id)
+                        print('did something')
+                    except tw.error.TweepError as e:
+                        print(e)
+
+if __name__ == '__main__':
+    foodbot = TwitterBot(keys['consumer_key'], keys['consumer_secret'],
+                         keys['access_token'], keys['access_token_secret'], 0)
+    foodbot.run_bot()
