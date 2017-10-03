e4c89173b329b913f6870d0e5ee53d0bd1a8bd76
John Barksdale <jdbarks@gmail.com>
2017-10-02 19:11:57 -0500

    Loaded data from json file.  Removed need to enter in max_id when running the foodbot.  Getting an error when running: tweepy.error.TweepError: [{'code': 44, 'message': 'since_id parameter is invalid.'}]

--- temp_json.py
+++ temp_json.py
@@ -1,30 +1,28 @@
 import json
+#
+# data = {}
+# data['people'] = []
+# data['people'].append({
+#     'name': 'Scott',
+#     'website': 'stackabuse.com',
+#     'from': 'Nebraska'
+# })
+# data['people'].append({
+#     'name': 'Larry',
+#     'website': 'google.com',
+#     'from': 'Michigan'
+# })
+# data['people'].append({
+#     'name': 'Tim',
+#     'website': 'apple.com',
+#     'from': 'Alabama'
+# })
+#
+# with open('data.txt', 'w') as outfile:
+#     json.dump(data, outfile, indent=4)
 
-data = {}
-data['people'] = []
-data['people'].append({
-    'name': 'Scott',
-    'website': 'stackabuse.com',
-    'from': 'Nebraska'
-})
-data['people'].append({
-    'name': 'Larry',
-    'website': 'google.com',
-    'from': 'Michigan'
-})
-data['people'].append({
-    'name': 'Tim',
-    'website': 'apple.com',
-    'from': 'Alabama'
-})
 
-with open('data.txt', 'w') as outfile:
-    json.dump(data, outfile, indent=4)
-
-with open('data.txt') as json_file:
-    data = json.load(json_file)
-    for p in data['people']:
-        print('Name: ' + p['name'])
-        print('Website: ' + p['website'])
-        print('From: ' + p['from'])
-        print('')
+def load_json():
+    with open('./data/data.json') as json_file:
+        data = json.load(json_file)
+    return data
